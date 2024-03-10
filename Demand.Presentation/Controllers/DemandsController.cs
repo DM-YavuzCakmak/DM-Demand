2377d7894e78365e7ffacd285c13df248ba6c2ac
Besim Oznalcin <besim.oznalcin@demmuseums.com>
2024-03-10 21:35:35 +0300

    Change

--- Demand.Presentation/Controllers/DemandsController.cs
+++ Demand.Presentation/Controllers/DemandsController.cs
@@ -13,6 +13,7 @@ using Demand.Domain.Entities.DemandProcess;
 using Demand.Domain.Entities.DepartmentEntity;
 using Demand.Domain.Entities.Personnel;
 using Demand.Domain.ViewModels;
+using Kep.Helpers.Extensions;
 using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
@@ -74,8 +75,20 @@ namespace Demand.Presentation.Controllers
                 UpdatedAt = demand.UpdatedAt,
                 UpdatedDate = demand.UpdatedDate,
                 CompanyName = company.Name,
-                DepartmentName = department.Name
+                DepartmentName = department.Name,
             };
+            if (demandMediaEntities.IsNotNullOrEmpty())
+            {
+                demandViewModel.File1Path = System.IO.File.ReadAllBytes(_webHostEnvironment.WebRootPath + demandMediaEntities[0].Path);
+                if (demandMediaEntities.Count > 1)
+                {
+                    demandViewModel.File2Path = System.IO.File.ReadAllBytes(_webHostEnvironment.WebRootPath + demandMediaEntities[1].Path);
+                }
+                if (demandMediaEntities.Count > 2)
+                {
+                    demandViewModel.File3Path = System.IO.File.ReadAllBytes(_webHostEnvironment.WebRootPath + demandMediaEntities[2].Path);
+                }
+            }
 
             List<Company> companies = _companyService.GetList().Data.ToList();
             ViewBag.Companies = companies;
@@ -305,6 +318,7 @@ namespace Demand.Presentation.Controllers
             return fileBytes;
         }
 
+
         private DemandMediaEntity SaveFileAndCreateEntity(IFormFile file, long demandId)
         {
             if (file != null && file.Length > 0)
@@ -321,7 +335,7 @@ namespace Demand.Presentation.Controllers
                 return new DemandMediaEntity
                 {
                     DemandId = demandId,
-                    Path = filePath
+                    Path = "\\uploads\\" + uniqueFileName
                 };
             }
 

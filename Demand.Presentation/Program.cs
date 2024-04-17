12d220f60198ce08347b88dfa67316c263bb0df9
SametBass <samet.bas@demmuseums.com>
2024-04-17 15:21:32 +0300

    DbConnection

--- Demand.Presentation/Program.cs
+++ Demand.Presentation/Program.cs
@@ -67,7 +67,7 @@ builder.Services.AddControllersWithViews();
 #region DbContext
 builder.Services.AddDbContext<DemandContext>(options =>
 {
-    options.UseSqlServer("Data Source=172.30.47.17,1433;Initial Catalog=Demand;User Id=sa;Password=123456;TrustServerCertificate=true;");
+    options.UseSqlServer("Data Source=172.30.196.15,1433;Initial Catalog=Demand;User Id=sa;Password=123456;TrustServerCertificate=true;");
 });
 #endregion
 

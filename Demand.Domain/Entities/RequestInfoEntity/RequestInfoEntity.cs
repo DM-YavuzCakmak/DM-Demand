42ad9100f58d8867dffe3f7f57dcc6f0cd3f3ce4
SametBass <samet.bas@demmuseums.com>
2024-09-12 15:18:53 +0300

    Commit

--- Demand.Domain/Entities/RequestInfoEntity/RequestInfoEntity.cs
+++ Demand.Domain/Entities/RequestInfoEntity/RequestInfoEntity.cs
@@ -14,10 +14,10 @@ namespace Demand.Domain.Entities.RequestInfoEntity
         public long DemandId { get; set; }
         public int? NebimCategoryId { get; set; }
         public int? NebimSubCategoryId { get; set; }
-        public string ProductName { get; set; }
+        public string? ProductName { get; set; }
         public string? ProductCode { get; set; }
         public int? Quantity { get; set; }
-        public string Unit { get; set; }
+        public string? Unit { get; set; }
         public bool  IsFirst { get; set; }
         public int? ProductCategoryId { get; set; }
     }
